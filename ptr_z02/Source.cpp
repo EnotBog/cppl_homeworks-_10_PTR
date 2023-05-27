f50849f7517a3b63dc3ced95f7cf758e0985a5e6
EnotBog <enotbog@gmail.com>
2023-05-27 21:44:10 +0300

    First commit

---
+++ ptr_z02/Source.cpp
@@ -0,0 +1,30 @@
+﻿#include <windows.h>
+#include <locale.h>
+#include <vector>
+#include <iostream>
+#include <iostream>
+#include <vector>
+#include <memory>
+
+class node
+{
+public:
+    int m_value;
+    std::weak_ptr<node> parent;
+    node(int value) : m_value{ value } {};
+    ~node() { std::cout << "destructor called\n"; }
+};
+ 
+
+int main()
+{
+	setlocale(LC_ALL, "Russian");
+	system("chcp 1251");
+  
+    auto node1 = std::make_shared<node>(1);
+    auto node2 = std::make_shared<node>(2);
+    node1->parent = node2;
+    node2->parent = node1;
+    
+    return 0;
+}
---
+++ ptr_z03/Source.cpp
@@ -0,0 +1,73 @@
+﻿#include <windows.h>
+#include <locale.h>
+#include <vector>
+#include <iostream>
+#include <memory>
+
+template <class T>
+class MyUnique
+{
+public:
+    MyUnique() {}
+    MyUnique(T* a) :ptr(a)
+    {
+    };
+    ~MyUnique()
+    {
+        std::cout << "Delete MyUnique\n";
+        delete ptr;
+    };
+
+    T* release() 
+    {
+        auto p = ptr;
+        ptr = NULL;
+        return p;
+    }
+
+    T operator *() { return *ptr; }
+
+    MyUnique(const MyUnique&) = delete;
+
+private:
+    
+    void operator = (const MyUnique a) = delete;
+
+    T* ptr=NULL;
+};
+
+class MyClass
+{
+public:
+    MyClass(int a): size(a)
+    {
+        P_arr= new int(size);
+        for (int i =0; i < size; ++i)
+        {
+            P_arr[i] = i;
+        }
+    }
+  
+    ~MyClass() 
+    { std::cout << "Delete MyClass\n";
+    delete P_arr;
+    }
+private:
+    int size = 0;
+    int* P_arr = NULL;
+};
+
+
+
+int main()
+{
+	setlocale(LC_ALL, "Russian");
+	system("chcp 1251");
+ 
+
+    MyUnique<MyClass> unique_int(new MyClass(5));
+  //  std::cout << *unique_int;
+    auto p = unique_int.release();
+    delete p;
+    return 0;
+}
